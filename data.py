17e0d8dd59d665d5bff1b56b0123f0a17ff9f653
Sarang Zambare <Wolf-Pack@Sarangs-MacBook-Air.local>
2018-12-29 21:44:48 -0800

    Solved issue regarding unicode to str conversion for some data fields.

--- data.py
+++ data.py
@@ -46,26 +46,26 @@ for brand_link in brand_links:
         # scraping all product specifications
         specs_list_container = soup.find('div',{'id':'specs-list'})
         product_lte = 1 if 'LTE' in str(specs_list_container.find('a',{'class':'link-network-detail collapse'}).text) else 0
-        product_year = str(specs_list_container.find('td',{'data-spec':'year'}).text) if specs_list_container.find('td',{'data-spec':'year'}) else None
-        product_weight = str(specs_list_container.find('td',{'data-spec':'weight'}).text) if specs_list_container.find('td',{'data-spec':'weight'}) else None
-        product_disp_type = str(specs_list_container.find('td',{'data-spec':'displaytype'}).text) if specs_list_container.find('td',{'data-spec':'displaytype'}) else None
-        product_disp_size = str(specs_list_container.find('td',{'data-spec':'displaysize'}).text) if specs_list_container.find('td',{'data-spec':'displaysize'}) else None
-        product_ppi = str(specs_list_container.find('td',{'data-spec':'displayresolution'}).text) if specs_list_container.find('td',{'data-spec':'displayresolution'}) else None
-        product_ghz = str(specs_list_container.find('td',{'data-spec':'cpu'}).text) if specs_list_container.find('td',{'data-spec':'cpu'}) else None
-        product_ram = str(specs_list_container.find('td',{'data-spec':'internalmemory'}).text) if specs_list_container.find('td',{'data-spec':'internalmemory'}) else None
-        product_internal = str(specs_list_container.find('td',{'data-spec':'internalmemory'}).text) if specs_list_container.find('td',{'data-spec':'internalmemory'}) else None
-        product_cam = str(soup.find('td',{'data-spec':'cam1modules'}).text[:10]) if soup.find('td',{'data-spec':'cam1modules'}) else None
-        product_frontcam = str(soup.find('td',{'data-spec':'cam2modules'}).text[:10]) if soup.find('td',{'data-spec':'cam2modules'}) else None
-        product_bluetooth = str(soup.find('td',{'data-spec':'bluetooth'}).text) if soup.find('td',{'data-spec':'bluetooth'}) else None
-        product_battery = str(soup.find('td',{'data-spec':'batdescription1'}).text) if soup.find('td',{'data-spec':'batdescription1'}) else None
-        product_price = str(soup.find('td',{'data-spec':'price'}).text) if soup.find('td',{'data-spec':'price'}) else None
+        product_year = str(specs_list_container.find('td',{'data-spec':'year'}).text.encode('ascii','ignore')) if specs_list_container.find('td',{'data-spec':'year'}) else None
+        product_weight = str(specs_list_container.find('td',{'data-spec':'weight'}).text.encode('ascii','ignore')) if specs_list_container.find('td',{'data-spec':'weight'}) else None
+        product_disp_type = str(specs_list_container.find('td',{'data-spec':'displaytype'}).text.encode('ascii','ignore')) if specs_list_container.find('td',{'data-spec':'displaytype'}) else None
+        product_disp_size = str(specs_list_container.find('td',{'data-spec':'displaysize'}).text.encode('ascii','ignore')) if specs_list_container.find('td',{'data-spec':'displaysize'}) else None
+        product_ppi = str(specs_list_container.find('td',{'data-spec':'displayresolution'}).text.encode('ascii','ignore')) if specs_list_container.find('td',{'data-spec':'displayresolution'}) else None
+        product_ghz = str(specs_list_container.find('td',{'data-spec':'cpu'}).text.encode('ascii','ignore')) if specs_list_container.find('td',{'data-spec':'cpu'}) else None
+        product_ram = str(specs_list_container.find('td',{'data-spec':'internalmemory'}).text.encode('ascii','ignore')) if specs_list_container.find('td',{'data-spec':'internalmemory'}) else None
+        product_internal = str(specs_list_container.find('td',{'data-spec':'internalmemory'}).text.encode('ascii','ignore')) if specs_list_container.find('td',{'data-spec':'internalmemory'}) else None
+        product_cam = str(soup.find('td',{'data-spec':'cam1modules'}).text.encode('ascii','ignore')) if soup.find('td',{'data-spec':'cam1modules'}) else None
+        product_frontcam = str(soup.find('td',{'data-spec':'cam2modules'}).text.encode('ascii','ignore')) if soup.find('td',{'data-spec':'cam2modules'}) else None
+        product_bluetooth = str(soup.find('td',{'data-spec':'bluetooth'}).text.encode('ascii','ignore')) if soup.find('td',{'data-spec':'bluetooth'}) else None
+        product_battery = str(soup.find('td',{'data-spec':'batdescription1'}).text.encode('ascii','ignore')) if soup.find('td',{'data-spec':'batdescription1'}) else None
+        product_price = str(soup.find('td',{'data-spec':'price'}).text.encode('ascii','ignore')) if soup.find('td',{'data-spec':'price'}) else None
 
         # appending product to products_df dataframe
         products_df.loc[products_df.shape[0],:] = [product_name,product_lte,product_year,product_weight,product_disp_type,product_disp_size,product_ppi,product_ghz,product_ram,product_internal,product_cam,product_frontcam,product_bluetooth,product_battery,product_price]
 
 
 
-products_df.to_csv('smartdevices_data.csv')
+products_df
 
 
 # re for ppi = re.findall(r'\d{1,3} ppi',str(specs_list_container.find('td',{'data-spec':'displayresolution'}).text))[0][:4]
