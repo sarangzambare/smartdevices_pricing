3db739cf2a7655985bc87fbe411dab48287828dd
Sarang Zambare <thesarng@gmail.com>
2018-12-30 20:34:28 -0800

    Added regex methods to extract column fields from strings

--- analy.py
+++ analy.py
@@ -66,6 +66,74 @@ for i in range(products_df.shape[0]):
     elif(len(re.findall(r'\d{0,3}\.{0,1}\d{0,2}\sMHz',products_df.iloc[i,7])) != 0):
         products_df.iloc[i,7] = "0." + re.findall(r'\d{0,3}\.{0,1}\d{0,2}\sMHz',products_df.iloc[i,7])[0][:-4].replace(".","")
     else:
-        products_df.iloc[i,7] = None
+        products_df.iloc[i,7] = 0
 
-                   
+# cleaning column ram, units=GB
+for i in range(products_df.shape[0]):
+    if(len(re.findall(r'\d{0,3}\.{0,1}\d{0,2}\sGB\sRAM',products_df.iloc[i,8])) != 0):
+        products_df.iloc[i,8] = re.findall(r'\d{0,3}\.{0,1}\d{0,2}\sGB\sRAM',products_df.iloc[i,8])[0][:-7]
+    elif(len(re.findall(r'\d{0,3}\.{0,1}\d{0,2}\sMB\sRAM',products_df.iloc[i,8])) != 0):
+        products_df.iloc[i,8] = "0." + re.findall(r'\d{0,3}\.{0,1}\d{0,2}\sMB\sRAM',products_df.iloc[i,8])[0][:-7].replace(".","")
+    else:
+        products_df.iloc[i,8] = 0
+
+
+# cleaning column internal, units=GB
+for i in range(products_df.shape[0]):
+       if(len(re.findall(r'\d{0,3}\.{0,1}\d{0,2}\sTB',products_df.iloc[i,9])) != 0):
+           if("." in re.findall(r'\d{0,3}\.{0,1}\d{0,2}\sTB',products_df.iloc[i,9])[0][:-3]):
+               products_df.iloc[i,9] = re.findall(r'\d{0,3}\.{0,1}\d{0,2}\sTB',products_df.iloc[i,9])[0][:-3].replace(".","") + "00"
+           else:
+               products_df.iloc[i,9] = re.findall(r'\d{0,3}\.{0,1}\d{0,2}\sTB',products_df.iloc[i,9])[0][:-3].replace(".","") + "000"
+       elif(len(re.findall(r'\d{0,3}\.{0,1}\d{0,2}\sGB',products_df.iloc[i,9])) != 0):
+            products_df.iloc[i,9] = re.findall(r'\d{0,3}\.{0,1}\d{0,2}\sGB',products_df.iloc[i,9])[0][:-3]
+
+       elif(len(re.findall(r'\d{0,3}\.{0,1}\d{0,2}\sMB',products_df.iloc[i,9])) != 0):
+           products_df.iloc[i,9] = "0." + re.findall(r'\d{0,3}\.{0,1}\d{0,2}\sMB',products_df.iloc[i,9])[0][:-3].replace(".","")
+
+
+# cleaning column camera, units=MP
+for i in range(products_df.shape[0]):
+    if(len(re.findall(r'\d{0,3}\.{0,1}\d{0,2}\sMP',products_df.iloc[i,10])) != 0):
+        products_df.iloc[i,10] = re.findall(r'\d{0,3}\.{0,1}\d{0,2}\sMP',products_df.iloc[i,10])[0][:-3]
+    else:
+        products_df.iloc[i,10] = 0
+
+# cleaning column front_cam, units=MP
+for i in range(products_df.shape[0]):
+    if(len(re.findall(r'\d{0,3}\.{0,1}\d{0,2}\sMP',products_df.iloc[i,11])) != 0):
+        products_df.iloc[i,11] = re.findall(r'\d{0,3}\.{0,1}\d{0,2}\sMP',products_df.iloc[i,11])[0][:-3]
+    else:
+        products_df.iloc[i,11] = 0
+
+
+# cleaning column blth_v, units=bluetooth version
+for i in range(products_df.shape[0]):
+    if(len(re.findall(r'\d\.\d',products_df.iloc[i,12])) != 0):
+        products_df.iloc[i,12] = re.findall(r'\d\.\d',products_df.iloc[i,12])[0]
+    else:
+        products_df.iloc[i,12] = 0
+
+
+# cleaning column battery, units=mAh
+for i in range(products_df.shape[0]):
+    if(len(re.findall(r'\d{0,5}\smAh',products_df.iloc[i,13])) != 0):
+        products_df.iloc[i,13] = re.findall(r'\d{0,5}\smAh',products_df.iloc[i,13])[0][:-4]
+    else:
+        products_df.iloc[i,13] = 0
+
+
+
+# cleaning column price, units=USD (current rate as of 12/30/2018)
+for i in range(products_df.shape[0]):
+    if(len(re.findall(r'\d{0,6}\sEUR',products_df.iloc[i,14])) != 0):
+        products_df.iloc[i,14] = float(re.findall(r'\d{0,6}\sEUR',products_df.iloc[i,14])[0][:-4])*1.14
+    elif(len(re.findall(r'\d{0,6}\sUSD',products_df.iloc[i,14])) != 0):
+        products_df.iloc[i,14] = float(re.findall(r'\d{0,6}\sUSD',products_df.iloc[i,14])[0][:-4])
+    elif(len(re.findall(r'\d{0,6}\sINR',products_df.iloc[i,14])) != 0):
+        products_df.iloc[i,14] = float(re.findall(r'\d{0,6}\sINR',products_df.iloc[i,14])[0][:-4])*0.014
+
+
+products_df = products_df.dropna(axis=0)
+
+products_df.to_csv('csv/data_clean.csv')
